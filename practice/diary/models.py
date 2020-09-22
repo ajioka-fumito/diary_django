50547325fecabd3d57e623c03d26f576da289b29
ajioka <ajioka.ds@gmail.com>
2020-09-22 14:36:41 +0900

    日記追加機能とindexページへの一覧表示機能の追加

--- practice/diary/models.py
+++ practice/diary/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Day(models.Model):
+    title = models.CharField('title',max_length=200)
+    text = models.TextField('text')
+    date = models.DateField('date',default=timezone.now)
--- practice/diary/views.py
+++ practice/diary/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Day 
+from .forms import DayCreateForm
 # Create your views here.
 
 def index(request):
-    return render(request,'diary/index.html')
+    context = {
+        'day_list':Day.objects.all(),
+    }
+    return render(request,'diary/index.html',context)
+
+def add(request):
+    form = DayCreateForm(request.POST or None)
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        return redirect('diary:index')
+    context = {
+        'form':form
+    }
+    return render(request,'diary/add.html',context)
